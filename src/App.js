70729f20401471e1cf3fd4fa8092b1ef10574a68
ryuimoto <narrow617@gmail.com>
2023-03-27 16:58:07 +0900

    Settings.jsを追加

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Settings from './Settings';
+import Timer from './Timer';
+import { useState } from 'react';
 
 function App() {
+  const [showSettings,setShowSettings] = useState(initialState:true);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <main>
+    {showSettings ? <Settings /> : <Timer />}
+    <Timer />
+   </main>
   );
 }
 
