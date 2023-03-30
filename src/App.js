7f7ff3a1fa990de75d834798a09c693eb18dad1d
ryuimoto <narrow617@gmail.com>
2023-03-30 17:15:19 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import './App.css';
 import Settings from './Settings';
 import Timer from './Timer';
 import { useState } from 'react';
+import SettingsContext from './SettingsContext';
 
 function App() {
-  const [showSettings,setShowSettings] = useState(initialState:true);
-
+  const [showSettings,setShowSettings] = useState(false);
+  const [workMinutes,setWorkMinutes] = useState(45);
+  const {breakMinutes,setBreakMinutes} = useState(15);
 
   return (
    <main>
-    {showSettings ? <Settings /> : <Timer />}
-    <Timer />
+    <SettingsContext.Provider value={{
+      showSettings,
+      setShowSettings,
+      workMinutes,
+      breakMinutes,
+      setWorkMinutes,
+      setBreakMinutes,
+    }}>
+      {showSettings ? <Settings /> : <Timer />}
+    </SettingsContext.Provider>
    </main>
   );
 }
--- src/Settings.js
+++ src/Settings.js
@@ -1,19 +1,40 @@
+import { useContext } from "react";
 import ReactSlider from "react-slider";
+import BackButton from "./BackButton";
+import SettingsContext from "./SettingsContext";
+import './slider.css'
+
 
 function Settings(){
+    const settingsInfo = useContext(SettingsContext);
+
     return(
         <div style={{textAlign:'left'}}>
-            <label>work minutes:</label>
+            <label>work: {settingsInfo.workMinutes}:00</label>
             <ReactSlider
                 className={'slider'}
                 thumbClassName={'thumb'}
                 trackClassName={'track'}
-                value={45}
+                value={settingsInfo.workMinutes}
+                onChange={newValue => settingsInfo.setWorkMinutes(newValue)} 
                 min={1}
                 max={120}
             />
-            <label>break minutes:</label>
+            <label>break: {settingsInfo.breakMinutes}:00</label>
+            <ReactSlider
+                className={'slider green'}
+                thumbClassName={'thumb'}
+                trackClassName={'track'}
+                value={settingsInfo.breakMinutes}
+                onChange={newValue => settingsInfo.setWorkMinutes(newValue)}
+                min={1}
+                max={120}
+            />
+            <div style={{textAlign:'center',marginTop:'20px'}}>
+                <BackButton onClick={() => settingsInfo.setShowSettings(false)} />
+            </div>
         </div>
+
     );
 }
 
